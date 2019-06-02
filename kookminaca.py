37f9a75ce4454254c268dbc158346054a062d457
goriver <50822293+goriver@users.noreply.github.com>
2019-06-03 00:42:12 +0900

    aca_second

--- kookminaca.py
+++ kookminaca.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
 
 class ScolarObj:
-    def __init__(self, article, writer, date, depart, attach, attach_link):
+    def __init__(self, article, title, writer, date, depart, attach, attach_link):
         # 파이썬 생성자
         self.article = article
+        self.title = title
         self.writer = writer
         self.date = date
         self.depart = depart
         self.attach = attach
         self.attach_link = attach_link
+
 
 
 
@@ -22,7 +26,7 @@ class ScolarCrawling:
     my_lists = soup.select(
         'td > a'
     )
-    scolarDetail=[]
+    scolar_details=[]
     for list in my_lists:
         # 세부 내용 긁어오기
         url = 'https://www.kookmin.ac.kr/site/resource/board/scholarship/' + list.get('href')
@@ -32,9 +36,9 @@ class ScolarCrawling:
         # boardview class
         board = tmp_soup.find("div", {"class": "boardview"})
         # 장학 공지사항 제목
-        article = tmp_soup.find(id='article_title')
+        title = tmp_soup.find(id='article_title')
 
-        tmp_article = article.text  ##
+        tmp_title = title.text
 
         # 장학 공지사항 글쓴이
         writer = board.select(
@@ -63,16 +67,21 @@ class ScolarCrawling:
         for at in attach:
             tmp_attach.append(at.text)
             tmp_attach_link.append(at.get('href'))
+        ## 장학 공지사항 내용
+        articles = tmp_soup.find(id='view-detail-data')
+        for article in articles:
+            print(article)
+            tmp_article = article
+        em = ScolarObj(tmp_article, tmp_title, tmp_writer, tmp_date, tmp_depart, tmp_attach, tmp_attach_link)
+        scolar_details.append(em)
 
-        em = ScolarObj( tmp_article, tmp_writer, tmp_date, tmp_depart, tmp_attach, tmp_attach_link)
-        scolarDetail.append(em)
+    for detail in scolar_details:
 
-    print(scolarDetail)
-   #[<__main__.ScolarObj object at 0x03312F10>, <__main__.ScolarObj object at 0x035FCC70>, <__main__.ScolarObj object at 0x03319390>, <__main__.ScolarObj object at 0x036053F0>, <__main__.ScolarObj object at 0x03319DB0>, <__main__.ScolarObj object at 0x035EB9F0>, <__main__.ScolarObj object at 0x03312BF0>, <__main__.ScolarObj object at 0x033BAA50>, <__main__.ScolarObj object at 0x0338E570>, <__main__.ScolarObj object at 0x0403BED0>, <__main__.ScolarObj object at 0x03F6CA30>, <__main__.ScolarObj object at 0x04043610>, <__main__.ScolarObj object at 0x042BBCF0>, <__main__.ScolarObj object at 0x041E9AF0>, <__main__.ScolarObj object at 0x04479F30>, <__main__.ScolarObj object at 0x0330EE50>, <__main__.ScolarObj object at 0x0447C670>]
+        print("{}".format(detail.article))
 
 
-    # 왜 이렇게 출력이 되죵,,,,,
-    # 그리고 list로 담아둔 것들을 그냥 파일로 저장하나요? sql같은거 안쓰고...??
+
+
 
 
 
