9b780969e69288e3f08525f4b926c586fab27341
goriver <50822293+goriver@users.noreply.github.com>
2019-06-05 17:47:57 +0900

    Add files via upload

--- include_Article.py
+++ include_Article.py
@@ -19,75 +19,76 @@ class ScolarObj:
 
 
 class ScolarCrawling:
-    # 학교 사이트의 url을 불러온다
 
 
-
-    req = requests.get('https://www.kookmin.ac.kr/site/resource/board/scholarship/')
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-    my_lists = soup.select(
-        'td > a'
-    )
-    scolarDetail=[]
-    for list in my_lists:
-        # 세부 내용 긁어오기
-        url = 'https://www.kookmin.ac.kr/site/resource/board/scholarship/' + list.get('href')
-        tmp_html = requests.get(url)
-        html = tmp_html.text
-        tmp_soup = BeautifulSoup(html, 'html.parser')
-        # boardview class
-        board = tmp_soup.find("div", {"class": "boardview"})
-        # 장학 공지사항 제목
-        title = tmp_soup.find(id='article_title')
-
-        tmp_title = title.text
-
-        # 장학 공지사항 글쓴이
-        writer = board.select(
-            '#content_body > section > div.boardview > table > tbody > tr:nth-child(1) > td:nth-child(4) > span'
+    # 학교 사이트의 url을 불러온다.
+    for i in range(57):
+        base_url = 'https://www.kookmin.ac.kr/site/resource/board/scholarship/?&pn={}'
+        page_url = base_url.format(i + 1 - 1)
+        req = requests.get(page_url)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        my_lists = soup.select(
+            'td > a'
         )
-        for w in writer:
-            tmp_writer =w.text
-        # 장학 공지사항 날짜
-        date = tmp_soup.select(
-            '#content_body > section > div.boardview > table > tbody > tr:nth-child(2) > td:nth-child(2)'
-        )
-        for d in date:
-            tmp_date =d.text
-        # 장학 공지사항 부서
-        depart = tmp_soup.select(
-            '#content_body > section > div.boardview > table > tbody > tr:nth-child(3) > td:nth-child(2)'
-        )
-        for de in depart:
-            tmp_depart=de.text
-        #장학 공지사항 첨부파일
-        attach = tmp_soup.select(
-            '#content_body > section > div.boardview > table > tbody > tr:nth-child(4) > td > a'
-        )
-        tmp_attach = []
-        tmp_attach_link=[]
-        for at in attach:
-            tmp_attach.append(at.text)
-            tmp_attach_link.append(at.get('href'))
+        scolarDetail = []
+        for list in my_lists:
+            # 세부 내용 긁어오기
+
+            url = 'https://www.kookmin.ac.kr/site/resource/board/scholarship/' + list.get('href')
+            tmp_html = requests.get(url)
+            html = tmp_html.text
+            tmp_soup = BeautifulSoup(html, 'html.parser')
+            # boardview class
+            board = tmp_soup.find("div", {"class": "boardview"})
+            # 장학 공지사항 제목
+            title = tmp_soup.find(id='article_title')
+
+            tmp_title = title.text
+            # 장학 공지사항 글쓴이
+            writer = board.select(
+                '#content_body > section > div.boardview > table > tbody > tr:nth-child(1) > td:nth-child(4) > span'
+            )
+            for w in writer:
+                tmp_writer = w.text
+            # 장학 공지사항 날짜
+            date = tmp_soup.select(
+                '#content_body > section > div.boardview > table > tbody > tr:nth-child(2) > td:nth-child(2)'
+            )
+            for d in date:
+                tmp_date = d.text
+            # 장학 공지사항 부서
+            depart = tmp_soup.select(
+                '#content_body > section > div.boardview > table > tbody > tr:nth-child(3) > td:nth-child(2)'
+            )
+            for de in depart:
+                tmp_depart = de.text
+            # 장학 공지사항 첨부파일
+            attach = tmp_soup.select(
+                '#content_body > section > div.boardview > table > tbody > tr:nth-child(4) > td > a'
+            )
+            tmp_attach = []
+            tmp_attach_link = []
+            for at in attach:
+                tmp_attach.append(at.text)
+                tmp_attach_link.append(at.get('href'))
+
+            # 장학 공지사항 내용
+            articles = tmp_soup.find(id='view-detail-data')
+            # 내용 이미지 담아놓는 tmp_article_image
+            article = articles.select('#view-detail-data > p > img')
+            for art_image in article:
+                tmp_article_image = art_image
+                # tmp_article_image +=arti_image로 하면 오류
+
+            # 내용 켁스트 담아놓는 tmp_
+            content = tmp_soup.find(id='view-detail-data').text
 
 
-        # 장학 공지사항 내용
-        articles = tmp_soup.find(id='view-detail-data')
-        # 내용 이미지 담아놓는 tmp_article_image
-        article = articles.select('#view-detail-data > p > img')
-        for art_image in article:
-            tmp_article_image = art_image
-            # tmp_article_image +=arti_image로 하면 오류
+            em = ScolarObj(tmp_title, tmp_writer, tmp_date, tmp_depart, tmp_attach,
+                           tmp_attach_link, tmp_article_image,content)
+            scolarDetail.append(em)
 
 
-        # 내용 켁스트 담아놓는 tmp_
-        contents = str(articles.find_all("p"))
-        contents = re.sub('<.+?>', '', contents, 0).strip()
-
-
-        em = ScolarObj(tmp_title, tmp_writer, tmp_date, tmp_depart, tmp_attach, tmp_attach_link, tmp_article_image,  contents.join('') )
-        scolarDetail.append(em)
-
-    for detail in scolarDetail:
-        print("{}".format(detail.article_text))
+        for detail in scolarDetail:
+            print("{}".format(detail.attach_link))
